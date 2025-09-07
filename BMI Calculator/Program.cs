5775cdd7ec35118e88788445195b4c617e2134e5
AkaMICKY <akaastroyx@gmail.com>
2025-09-07 18:15:03 +0100

    Add project files.

---
+++ BMI Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What Is Your Weight(Kg)? : ");
+            string Mass = Console.ReadLine();
+            double MASS = double.Parse(Mass);
+            Console.Write("What is Height(Meters)? : ");
+            string Height = Console.ReadLine();
+            double HEIGHT = double.Parse(Height);
+            double HEIGHT2 = Math.Pow(HEIGHT, 2);
+            double BMI = MASS /HEIGHT2;
+            double BMI_round = Math.Round(BMI, 2);
+            Console.WriteLine("Your BMI Is " + BMI_round);
+            Console.WriteLine("If Your BMI Is <18.5 = UNDER-WEIGT");
+            Console.WriteLine("If Your BMI Is 18.5-24.9 = HEALTHY WEIGHT ");
+            Console.WriteLine("If Your BMI Is 25-29.9 = OVER-WEIGHT");
+            Console.WriteLine("If Your BMI Is >30 , You Are OBESE!!!");
+        }
+    }
+}
