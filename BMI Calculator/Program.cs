63918f1c7d45f7051a1f611dc97b1c67d0538852
AkaMICKY <akaastroyx@gmail.com>
2025-09-12 19:46:14 +0100

    Added Failsafes and optimized code

--- BMI Calculator/Program.cs
+++ BMI Calculator/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,39 +14,32 @@ namespace BMI_Calculator
             Console.WriteLine("Choose an Option to Calculate Your BMI");
             Console.WriteLine("1. Weight-Kilograms(KG) and Height-Meters(M)");
             Console.WriteLine("2. Weight-Pounds(Lbs) and Height-Inches(Inch)");
+            Console.Write("--Option: ");
             string choice = Console.ReadLine(  );
             if (int.TryParse(choice, out int CHOICE) )
             {
                 if (CHOICE == 1)
                 {
                     Console.Write("What Is Your Weight(Kg)? : ");
-                    string Mass = Console.ReadLine();
-                    if (double.TryParse(Mass, out double MASS))
+                    string Mass_kgm = Console.ReadLine();
+                    if (double.TryParse(Mass_kgm, out double MASS_KGM))
                     {
                         Console.Write("What is Height(Meters)? : ");
-                        string Height = Console.ReadLine();
-                        if (double.TryParse(Height, out double HEIGHT))
+                        string Height_kgm = Console.ReadLine();
+                        if (double.TryParse(Height_kgm, out double HEIGHT_KGM))
                         {
-                            double HEIGHT2 = Math.Pow(HEIGHT, 2); // SQUARES HEIGHT(HEIGHT*HEIGHT)
-                            double BMI = MASS / HEIGHT2; //Divides MASS By Square of HEIGHT(HEIGHT2) 
-                            double BMI_round = Math.Round(BMI, 2); //Rounds BMI to 2 Decimal Places
-                            Console.WriteLine("Your BMI Is " + BMI_round);
-                            if (BMI_round < 18.5)
+                            if (MASS_KGM <= 0 || HEIGHT_KGM <= 0)
                             {
-                                Console.WriteLine("You are Under-Weight");
-                            }
-                            else if (BMI_round > 18.5 && BMI_round < 24.9)
+                                Console.WriteLine("Invalid Input(s), Value(s) must be Positive and Non-Zero");
+                            } else
                             {
-                                Console.WriteLine("You are Healthy :)");
+                                double HEIGHT2_KGM = Math.Pow(HEIGHT_KGM, 2); // SQUARES HEIGHT(HEIGHT*HEIGHT)
+                                double BMI_KGM = MASS_KGM / HEIGHT2_KGM; //Divides MASS By Square of HEIGHT(HEIGHT2) 
+                                double BMI_round = Math.Round(BMI_KGM, 2); //Rounds BMI to 2 Decimal Places
+                                Console.WriteLine("Your BMI Is " + BMI_round);
+                                PrintBMIClass(BMI_KGM);
                             }
-                            else if (BMI_round > 25 && BMI_round < 29.9)
-                            {
-                                Console.WriteLine("You are Over-Weight");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You are Obese!!");
-                            }
+                            
                         } else
                         {
                             Console.WriteLine("Invalid Input, Please Input the Correct Values");
@@ -62,33 +54,29 @@ namespace BMI_Calculator
                 else if (CHOICE == 2)
                 {
                     Console.Write("What Is Your Weight(Lbs)? : ");
-                    string Mass = Console.ReadLine();
-                    if (double.TryParse(Mass, out double MASS))
+                    string Mass_lbsinch = Console.ReadLine();
+                    if (double.TryParse(Mass_lbsinch, out double MASS_LBSINCH))
                     {
                         Console.Write("What is Height(Inches)? : ");
-                        string Height = Console.ReadLine();
-                        if (double.TryParse(Height, out double HEIGHT))
+                        string Height_lbsinch = Console.ReadLine();
+                        if (double.TryParse(Height_lbsinch, out double HEIGHT_LBSINCH))
                         {
-                            double HEIGHT2 = Math.Pow(HEIGHT, 2); // SQUARES HEIGHT(HEIGHT*HEIGHT)
-                            double BMI = (MASS / HEIGHT2) * 703; //Divides MASS By Square of HEIGHT(HEIGHT2) 
-                            double BMI_round = Math.Round(BMI, 2); //Rounds BMI to 2 Decimal Places
-                            Console.WriteLine("Your BMI Is " + BMI_round);
-                            if (BMI_round < 18.5)
+                            if (MASS_LBSINCH <= 0 || HEIGHT_LBSINCH <= 0)
                             {
-                                Console.WriteLine("You are Under-Weight");
-                            }
-                            else if (BMI_round > 18.5 && BMI_round < 24.9)
+                                Console.WriteLine("Invalid Input(s), Values Must Be Positive and Non-Zero");
+                            } else
                             {
-                                Console.WriteLine("You are Healthy :)");
+                                double HEIGHT2_LBSINCH = Math.Pow(HEIGHT_LBSINCH, 2); // SQUARES HEIGHT(HEIGHT*HEIGHT)
+                                double BMI_LBSINCH = (MASS_LBSINCH / HEIGHT2_LBSINCH) * 703; //Divides MASS By Square of HEIGHT(HEIGHT2) 
+                                double BMI_round = Math.Round(BMI_LBSINCH, 2); //Rounds BMI to 2 Decimal Places
+                                Console.WriteLine("Your BMI Is " + BMI_round);
+                                PrintBMIClass(BMI_LBSINCH);
                             }
-                            else if (BMI_round > 25 && BMI_round < 29.9)
-                            {
-                                Console.WriteLine("You are Over-Weight");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You are Obese!!");
-                            }
+                            
+                            
+                        } else
+                        {
+                            Console.WriteLine("Invalid Input, Please Input the Correct Values");
                         }
                         
                     } else
@@ -99,16 +87,36 @@ namespace BMI_Calculator
                 }
                 else
                 {
-                    Console.WriteLine("Please Pick a Real Option " + CHOICE + " is not an Option");
+                    Console.WriteLine("Please Pick a Real Option, " + CHOICE + " is not an Option");
                 }
             }
             else
             {
-                 Console.WriteLine("Invalid Input, Please Enter the Correct Values");
+                 Console.WriteLine("Invalid Input, please enter a number (1 or 2).");
             }
 
+            Console.WriteLine("Press any key + Enter to exit...");
+            Console.ReadLine();
+        }
 
-
+        static void PrintBMIClass (double bmi)
+        {
+            if (bmi <= 18.5)
+            {
+                Console.WriteLine("Therefore, You are Under-Weight");
+            }
+            else if (bmi <= 25)
+            {
+                Console.WriteLine("Therefore, You are Healthy :)");
+            }
+            else if (bmi <= 30)
+            {
+                Console.WriteLine("Therefore, You are Over-Weight");
+            }
+            else
+            {
+                Console.WriteLine("Therefore, You are Obese!!");
+            }
         }
     }
 }
